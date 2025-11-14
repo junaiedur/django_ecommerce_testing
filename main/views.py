521a885769b419d92fcdde97cd5ddec48b06173f
MD Junaiedur Rahman <148237000+junaiedur@users.noreply.github.com>
2025-11-14 18:29:01 +0600

    new card ui

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 # Create your views here.
 
 def home(request):
-    return render(request, 'return.html')
+    return render(request, 'product_overview.html')
 
 def search(request):
     query = request.GET.get('q')
