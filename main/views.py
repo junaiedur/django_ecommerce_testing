a78e8e8b9b813081b80db1bad584666a86160703
MD Junaiedur Rahman <148237000+junaiedur@users.noreply.github.com>
2025-11-14 17:22:14 +0600

    icon changed, return page ccreated

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 # Create your views here.
 
 def home(request):
-    return render(request, 'order.html')
+    return render(request, 'return.html')
 
 def search(request):
     query = request.GET.get('q')
